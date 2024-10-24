673ce0a2957179a0ed02f34aba16d7badedfb671
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-24 21:51:50 +0200

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -6,7 +6,8 @@ import yaml
 from nodezator import Nodezator
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@localhost/threagile_db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@localhost/threagile_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
